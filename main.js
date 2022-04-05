565196a01e2eeb4e4083a5f6f4dd5a588447f877
Aleksy Oleksy <aleksyoleksy@MacBook-Pro-Aleksy.local>
2022-04-05 10:56:38 +0200

    design fixes, before and after ideas

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset
-window.onscroll = function () {
-	var currentScrollPos = window.pageYOffset
-	if (prevScrollpos > currentScrollPos) {
-		document.getElementById('navbar').style.top = '0'
-	} else {
-		document.getElementById('navbar').style.top = '-50px'
-	}
-	prevScrollpos = currentScrollPos
+const footerYear = document.querySelector('.footer__year')
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year + ' Smakager'
 }
+handleCurrentYear()
