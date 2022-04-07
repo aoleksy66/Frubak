e495f8c41b01927bd2f6c6cb05742de362a1c0d1
Aleksy Oleksy <aleksyoleksy@MacBook-Pro-Aleksy.local>
2022-04-07 09:51:30 +0200

    added hiding of the nav

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const nav = document.querySelector('.nav')
+
 const footerYear = document.querySelector('.footer__year')
 
 const handleCurrentYear = () => {
@@ -5,3 +7,12 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year + ' Smakager'
 }
 handleCurrentYear()
+
+function hideNav() {
+	if (window.scrollY >= 50) {
+		nav.classList.add('hide-nav')
+	} else {
+		nav.classList.remove('hide-nav')
+	}
+}
+window.addEventListener('scroll', hideNav)
